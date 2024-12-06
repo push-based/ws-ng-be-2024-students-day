a13bcc32b2c226199f006361937bb54cce1b1b9e
ChristopherPHolder <toholderandrews@gmail.com>
2024-12-06 10:58:04 +0100

    ex. 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,35 @@ import { AppShellComponent } from './app-shell/app-shell.component';
   imports: [AppShellComponent],
   template: `
     <app-shell>
-      <!-- app code here ;) -->
+      <div class="movie-card">
+        <img
+          class="movie-image"
+          [alt]="movie.title"
+          [src]="'https://image.tmdb.org/t/p/w342' + movie.poster_path" />
+        <div class="movie-card-content">
+          <div class="movie-card-title">
+            {{ movie.title }}
+          </div>
+          <div class="movie-card-rating">
+            {{ movie.vote_average }}
+          </div>
+        </div>
+        <button
+          class="favorite-indicator"
+          (click)="toggleFavorite(movie)"></button>
+      </div>
     </app-shell>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  movie = {
+    id: 'the-god',
+    title: 'The Godfather',
+    poster_path: '/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
+    vote_average: 10,
+  };
+
+  toggleFavorite(movie) {
+    console.log('toggled', movie);
+  }
+}
